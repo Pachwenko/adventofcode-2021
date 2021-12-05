2adae63705b01413f85ecc96b567706e641d3579
Pachwenko <32424503+Pachwenko@users.noreply.github.com>
2021-12-05 11:18:20 -0600

    finish day2 part2

---
+++ solutions/day2/day2part2.py
@@ -0,0 +1,39 @@
+FORWARD = "forward"
+DOWN = "down"
+UP = "up"
+
+
+def main(input_lines):
+    aim = 0
+    x = 0
+    y = 0
+    for line in input_lines:
+        split = line.split(" ")
+        operation = split[0]
+        value = int(split[1])
+        if operation == FORWARD:
+            x += value
+            y += aim * value
+        if operation == DOWN:
+            aim += value
+        if operation == UP:
+            aim -= value
+    return x * y
+
+
+def test_day2_simple():
+    input_lines = [
+        "forward 5",
+        "down 5",
+        "forward 8",
+        "up 3",
+        "down 8",
+        "forward 2",
+    ]
+    assert 900 == main(input_lines)
+
+
+def test_real_data():
+    with open('solutions/day2/input.txt') as f:
+        input_lines = f.readlines()
+        assert 1960569556 == main(input_lines)
